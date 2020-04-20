296881b57e6211ce09388b353333eb8374eea50b
GPU Job Config Builder <fyp_bot@github.com>
2020-04-20 20:57:20 +0000

    job_submission build

---
+++ job_submission.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1,gpu2
+#SBATCH --mail-type=NONE # required to send email notifcations
+#SBATCH --output=hello-cluster-world1.out
+#SBATCH --job-name=hello-cluster-world1
+export PATH=/vol/bitbucket/${USER}/miniconda3/bin/:$PATH
+source activate
+source /vol/cuda/10.0.130/setup.sh
+TERM=vt100 # or TERM=xterm
+python model.py
+/usr/bin/nvidia-smi
+uptime
