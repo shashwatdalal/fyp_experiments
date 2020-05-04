8a3e8151e648d2c6fbeee93cd33dd83961b1b82e
GPU Job Config Builder <fyp_bot@github.com>
2020-05-04 15:18:31 +0000

    job_config build

---
+++ job_config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+    #SBATCH --gres=gpu:1
+    #SBATCH --mail-type=NONE # required to send email notifcations
+    #SBATCH --output=reddit_comments_test.out
+    #SBATCH --job-name=reddit_comments_test
+    export PATH=/vol/bitbucket/${USER}/miniconda3/bin/:$PATH
+    source activate
+    source /vol/cuda/10.0.130/setup.sh
+    TERM=vt100 # or TERM=xterm
+    python model.py
+    /usr/bin/nvidia-smi
+    uptime
