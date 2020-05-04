cc7bba2b43f77ea93d2420a756487f520117eefc
Shashwat Dalal <spd16@ic.ac.uk>
2020-05-04 15:07:12 +0100

    Add GPU support

--- main.py
+++ main.py
@@ -11,6 +11,23 @@ from tqdm import tqdm, trange
 import yaml
 
 TQDM = False
+GPU = True
+
+
+def configure_cuda():
+    device_idx = 0
+    if GPU:
+        device = torch.device("cuda:" + str(device_idx) if torch.cuda.is_available() else "cpu")
+    else:
+        device = torch.device("cpu")
+    print(device)
+
+    # We set a random seed to ensure that your results are reproducible.
+    if torch.cuda.is_available():
+        torch.backends.cudnn.deterministic = True
+    torch.manual_seed(0)
+    return device
+
 
 if __name__ == '__main__':
 
@@ -21,6 +38,9 @@ if __name__ == '__main__':
     clients: List[str] = None
     dataset: FederatedDataset = None
     server_model: nn.Module = None
+
+    # configure GPU Settings
+    device = configure_cuda()
 
     if parameters['data']['dataset'] == 'Reddit Comments':
         # download data
@@ -54,58 +74,56 @@ if __name__ == '__main__':
             n_layers=parameters['language_model']['n_layers'],
             batch_size=parameters['language_model']['batch_size'],
         )
+
     else:
         raise KeyError('{} does not exist. Current Supported Datasets: "Redd'.format(parameters['data']['dataset']))
 
     # start training
     for round in trange(parameters['federated_parameters']['n_rounds'], position=0, desc="Rounds", disable=not TQDM):
-        for name, param in server_model.named_parameters():
-            print(round, name, torch.norm(param.detach(), 2).item())
+
         client_updates = {name: torch.zeros(len(clients), *param.shape) for name, param in
                           server_model.named_parameters()}
-
-        client_model = copy.deepcopy(server_model)
 
         # perform training
         for i, client in enumerate(tqdm(clients, position=1, leave=False, desc="Clients", disable=not TQDM)):
 
             # 'send' client server model
-            # todo using `state_dict()` and `load_state_dict()`
-            # fixme RuntimeError: Only Tensors created explicitly by the user (graph leaves) support the deepcopy protocol at the moment
+            client_model = copy.deepcopy(server_model).to(device)
 
             # initialize optimizer and loss function
-            client_optimizer = optim.Adam(client_model.parameters())
+            client_optimizer = optim.Adam(client_model.parameters(), lr=0.01)
             loss_fn = nn.CrossEntropyLoss()
 
             # perform local update
+            train_loss, test_loss = [], []
             for epoch in trange(parameters['federated_parameters']['n_epochs'],
                                 position=2, leave=False, desc="Epochs", disable=not TQDM):
                 train_iter, test_iter = dataset[client]
-                train_loss, test_loss = [], []
                 for batch in tqdm(train_iter, position=3, leave=False, desc="Train Batch", disable=not TQDM):
                     client_optimizer.zero_grad()
-                    text, target = batch.text, batch.target
+                    text, target = batch.text.to(device), batch.target.to(device)
                     predictions, _ = client_model(text, client_model.init_hidden())
                     loss = loss_fn(predictions, target.view(-1))
-                    train_loss.append(loss)
+                    train_loss.append(loss.item())
                     loss.backward()
                     client_optimizer.step()
+            print(round, client, sum(train_loss) / len(train_loss))
 
-                # calculate test loss
-                for batch in tqdm(test_iter, position=3, leave=False, desc="Test Batch", disable=not TQDM):
-                    with torch.no_grad():
-                        text, target = batch.text, batch.target
-                        predictions, _ = client_model(text, client_model.init_hidden())
-                        test_loss.append(loss_fn(predictions, target.view(-1)))
+                # # calculate test loss
+                # for batch in tqdm(test_iter, position=3, leave=False, desc="Test Batch", disable=not TQDM):
+                #     with torch.no_grad():
+                #         text, target = batch.text, batch.target
+                #         predictions, _ = client_model(text, client_model.init_hidden())
+                #         test_loss.append(loss_fn(predictions, target.view(-1)))
 
                 # todo log test/train loss
 
             # 'send' server update
             for (name, client_param), server_param in zip(client_model.named_parameters(), server_model.parameters()):
-                client_updates[name][i] = server_param.detach() - client_param.detach()
+                client_updates[name][i] = client_param.detach().cpu() - server_param.detach()
+            # print('update', client, size_of_update)
 
         # aggregate model
-        # fixme not updating the weight
         with torch.no_grad():
-            for name, param in client_updates.items():
-                setattr(server_model, name, torch.mean(param, dim=0))
+            for name, server_param in server_model.named_parameters():
+                server_param.data = server_param.data + torch.mean(client_updates[name], dim=0)
